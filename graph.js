f4786ee6ed8182bdb5a22099c5c97c5949ed363e
Kashif1Naqvi <naqvishah582@gmail.com>
2019-11-13 11:20:31 +0500

    project completed

--- graph.js
+++ graph.js
@@ -9,6 +9,7 @@ const graph = svg.append("g")
                .attr("height",graphHeight)
                .attr("width",graphWidth)
                .attr("transform",`translate(${margin.left},${margin.top})`);
+const tip = d3.select("body").append("div").attr("class","tip")
 
 const x = d3.scaleTime().range([0,graphWidth])
 const y = d3.scaleLinear().range([graphHeight,0])
@@ -26,6 +27,11 @@ const line = d3.line()
              .y(function(d){
                return y(d.distance)
              });
+const dottedLine  = graph.append("g").attr("class","line").style("opacity",0)
+
+const xDottedLine = dottedLine.append("line").attr("stroke","#aaa").attr("stroke-width",1).attr("stroke-dasharray",4)
+const yDottedLine = dottedLine.append("line").attr("stroke","#aaa").attr("stroke-width",1).attr("stroke-dasharray",4)
+
 const update = (data) => {
   data =  data.filter(item => item.activity == activity )
   data.sort((a,b)=> new Date(a.date) - new Date(b.date))
@@ -52,7 +58,48 @@ const update = (data) => {
        .attr("cy",d=>y(d.distance))
        .attr("fill","#fff");
 
+graph.selectAll("circle")
+      .on("mouseover",(d,i,n)=>{
+        tip.html("<div>"+ d.distance +"m</div><br><p>"+d.date+"</p>")
+        .style("top" , function(d,i){
+          return (d3.event.pageY-125)+i+"px"
+        })
+        .style("left",function(d,i){
+          return (d3.event.pageX-45)+i+ "px"
+        })
+        .transition()
+        .duration(700)
+        .delay(i*20)
+        .style('opacity',.9)
+        d3.select(n[i])
+          .transition()
+          .duration(100)
+          .attr("r",8)
+          .attr("fill","#fff")
 
+          xDottedLine
+            .attr("x1",x(new Date(d.date)))
+            .attr("x2",x(new Date(d.date)))
+            .attr("y1",graphHeight)
+            .attr("y2",y(d.distance))
+          yDottedLine
+            .attr("x1",0)
+            .attr("x2",x(new Date(d.date)))
+            .attr("y1",y(d.distance))
+            .attr("y2",y(d.distance))
+
+            dottedLine.style("opacity",1)
+      })
+      .on("mouseout",(d,i,n)=>{
+        tip.html("")
+        d3.select(n[i])
+          .transition()
+          .duration(100)
+          .attr("r",4)
+          .attr("fill","#fff");
+          dottedLine.style("opacity",0)
+
+      })
 const xAxis = d3.axisBottom(x)
                 .ticks(6)
                 .tickFormat(d3.timeFormat('%b %d'));
